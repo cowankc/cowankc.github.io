9b91a4a88fede56960f4f1ba2b479125ffc8c211
cowankc <cowankc92@live.com>
2020-01-23 21:12:23 -0600

    Updates

---
+++ precache-manifest.53a7578935e0686c92e71944dbe08238.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb51808a03aa68b1aa58db9a42293abd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cff8fbccc8d3b5da524d",
+    "url": "/static/css/main.1224d19a.chunk.css"
+  },
+  {
+    "revision": "21529216abd0c783decd",
+    "url": "/static/js/2.8a61d02a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8a61d02a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cff8fbccc8d3b5da524d",
+    "url": "/static/js/main.a97fa213.chunk.js"
+  },
+  {
+    "revision": "1bcf35cfa6bd54b9ddb6",
+    "url": "/static/js/runtime-main.996e8249.js"
+  },
+  {
+    "revision": "6504ba37ff99f56fc6592dc95c4710c5",
+    "url": "/static/media/LGO.6504ba37.png"
+  },
+  {
+    "revision": "c03b9959e3b3c3549604784ae6830727",
+    "url": "/static/media/Resume.c03b9959.pdf"
+  },
+  {
+    "revision": "d79b78f22cf42c92195a33d7350fe162",
+    "url": "/static/media/SP.d79b78f2.png"
+  },
+  {
+    "revision": "4e14e544d11bf7997daaa477dc50abab",
+    "url": "/static/media/burger.4e14e544.jpg"
+  },
+  {
+    "revision": "88537f3d63d6b3a4d0f49cc3a76bfd5a",
+    "url": "/static/media/code.88537f3d.jpg"
+  },
+  {
+    "revision": "361fe1da0f53f061ca91aacc18e2c53d",
+    "url": "/static/media/hangman.361fe1da.png"
+  },
+  {
+    "revision": "dbaaea10a04f5bd4393c0ff2e09b7516",
+    "url": "/static/media/me.dbaaea10.jpg"
+  },
+  {
+    "revision": "9137caeac778823cb2f54f6a20d9b55d",
+    "url": "/static/media/rpg.9137caea.png"
+  },
+  {
+    "revision": "3d2385a504cc572aed53199cc872a125",
+    "url": "/static/media/rps.3d2385a5.jpg"
+  },
+  {
+    "revision": "6b32d387c2218514084619b57a77e226",
+    "url": "/static/media/trivia.6b32d387.png"
+  },
+  {
+    "revision": "6bdc0b78bdc45914c7ce197932bdb866",
+    "url": "/static/media/whatscookin.6bdc0b78.png"
+  }
+]);
